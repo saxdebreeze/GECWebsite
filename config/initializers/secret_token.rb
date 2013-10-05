47878a6b598fa0b2b22a4796bc8ea22847374243
Po-Cheng Pan <saxdebreeze@gmail.com>
2013-10-05 14:19:22 +0800

    add as Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GECWebsite::Application.config.secret_key_base = 'e9334052248d88ccf99a29e895944c28bffd2f8027e1d250287daa16694805ffd10a1a2155fd4cdfa39c903867fdb6763bf0faf59b3f1c0b22a9b25726f3fd27'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GECWebsite::Application.config.session_store :cookie_store, key: '_GECWebsite_session'
